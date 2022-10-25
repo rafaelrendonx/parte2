8aad63f7ca51f2ca1ce04926776468badb4c6494
rafaelrendonx <rafael_x@hotmail.com>
2022-10-25 16:55:28 -0700

    Conectar con backend en nube

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://keen-dodol-3a3cac.netlify.app/.netlify/functions/api/api/persons'
 
 const getAll = () => {
     return axios.get(baseUrl)
